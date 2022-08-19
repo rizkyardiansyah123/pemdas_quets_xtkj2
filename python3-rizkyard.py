570a12e9d94d7d804a47770fdc07ef9b9331ec12
rizkyardiansyah123 <111480121+rizkyardiansyah123@users.noreply.github.com>
2022-08-19 15:00:45 +0700

    Add files via upload

---
+++ python3-rizkyard.py
@@ -0,0 +1,17 @@
+nama_depan= input("masukan nama depan:")
+nama_belakang= input("masukan nama belakang:")
+print("nama lengkap saya", nama_depan,nama_belakang)
+
+a= int(input("masukan nilai A: "))
+b= float(input("masukan nilain b: "))
+c=a+b
+ 
+print("a=", a)
+print("b=", b)
+print(a,"+",b,"=", c)
+
+# Lengkapi Script Berikut Ini
+
+x =  int(input("masukan suhu celcius="))
+y = (9/5 * x) + 32
+print(x, "celcius = ", y, "fahreheit")
